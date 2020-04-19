691f72d7a1cc5966863195d50c23af7e7c0fc2be
Axel Boldt-Christmas <xmas1915@gmail.com>
2020-04-19 08:41:43 +0200

    Testing

--- test/setup.sh
+++ test/setup.sh
@@ -1,7 +1,7 @@
 echo "Test"
 touch test
 echo "[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]]" > test
-tar -C /gitlab -xzvf /gitlab/test.tar.gz &> test
+tar -C /gitlab -xzvf ~/gitlab/test.tar.gz &> test
 docker run --detach \
   --hostname [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com \
   --env GITLAB_OMNIBUS_CONFIG="external_url 'https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/'; gitlab_rails['initial_root_password'] = 'password';" \
