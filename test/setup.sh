01eb78f724f7d014f2f53a37e377ae0ee3ba1ac1
Axel Boldt-Christmas <xmas1915@gmail.com>
2020-04-19 10:00:36 +0200

    Update setup.sh

--- test/setup.sh
+++ test/setup.sh
@@ -1,16 +1,10 @@
 echo "Test"
 touch test
 echo "[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]]" > test
-git config --global user.name "Me"
-git config --global user.email "user@example.com"
-git clone https://github.com/xmas92/katacoda-scenarios.git tmp
-mkdir /gitlab
-tar -C /gitlab -xzvf tmp/test/assets/test.tar.gz
-rm -rf tmp
 echo "Run Docker" >> test
 docker run --detach \
   --hostname [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com \
-  --env GITLAB_OMNIBUS_CONFIG="external_url 'https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/';" \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/'; gitlab_rails['initial_root_password'] = 'password';"" \
   --publish 443:443 --publish 80:80 --publish 2289:22 \
   --name gitlab \
   --restart always \
@@ -18,16 +12,4 @@ docker run --detach \
   --volume /gitlab/logs:/var/log/gitlab \
   --volume /gitlab/data:/var/opt/gitlab \
   gitlab/gitlab-ce:latest || true
-echo "Sleep" >> test
-sleep 2
-echo "update permissions" >> test
-docker exec -it gitlab update-permissions
-echo "restart" >> test
-docker restart gitlab
-echo "sleep" >> test
-sleep 2
-echo "fix gitaly" >> test
-docker exec -it gitlab chown -R git:git /var/opt/gitlab/gitaly/
-echo "restart" >> test
-docker restart gitlab
 echo "Done" >> test
