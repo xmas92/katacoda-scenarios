8fbfd1a03acb761f1df748af7066b161715361b5
Axel Boldt-Christmas <xmas1915@gmail.com>
2020-04-25 15:57:05 +0200

    Update setup.sh

--- gitlab-bot/setup.sh
+++ gitlab-bot/setup.sh
@@ -1,9 +1,4 @@
-echo "Test"
-touch test
-echo "Update node" >> test
 npx n lts
-echo "[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]]" > test
-echo "Run Docker" >> test
 docker run --detach \
   --hostname [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com \
   --env GITLAB_OMNIBUS_CONFIG="external_url 'https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/'; gitlab_rails['initial_root_password'] = 'password';" \
@@ -14,4 +9,3 @@ docker run --detach \
   --volume /gitlab/logs:/var/log/gitlab \
   --volume /gitlab/data:/var/opt/gitlab \
   gitlab/gitlab-ce:latest
-echo "Done" >> test
